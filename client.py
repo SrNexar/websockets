bdabfae21531f4cf4c70165fdee4e2dc482cf379
SrNexar <nfsalguero@uce.edu.ec>
2024-05-25 16:44:34 -0500

    mensaje predefinido

--- client.py
+++ client.py
@@ -3,14 +3,10 @@ import websockets
 
 async def send_message():
     async with websockets.connect("ws://localhost:8765") as websocket:
-        while True:
-            # Envía un mensaje al servidor
-            message = input("Ingrese un mensaje: ")
-            await websocket.send(message)
-            
-            # Espera la respuesta del servidor
-            response = await websocket.recv()
-            print(f"Respuesta del servidor: {response}")
+        message = "Mensaje automático desde GitHub Actions"
+        await websocket.send(message)
+        response = await websocket.recv()
+        print(f"Respuesta del servidor: {response}")
 
-asyncio.get_event_loop().run_until_complete(send_message())
-#hial
+if __name__ == "__main__":
+    asyncio.run(send_message())
